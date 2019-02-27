20db5a03555a5b9e131f022f936efb673d427a6f
JulieFournier <julie.fournier@laposte.net>
2019-02-27 15:58:23 +0100

    Ajout des fonctions de calcul. Fonction add, sub, mutl, div dans CalcultariceNormale et exp, sqrt, pow dans CalcultriceScientifique (mais gros problèmes àl'exécution)

--- calculatrice/CalculatriceNormale.h
+++ calculatrice/CalculatriceNormale.h
@@ -1,8 +1,35 @@
 #pragma once
 
 class CalculatriceNormale {
+protected :
+	float m_result;
 public:
-	CalculatriceNormale() {
+	CalculatriceNormale() : m_result(0) {
 		
 	}
+
+	float add(float f1, float f2) {
+		m_result = f1 + f2;
+		return m_result;
+	}
+
+	float sub(float f1, float f2) {
+		m_result = f1 - f2;
+		return m_result;
+	}
+
+	float mult(float f1, float f2) {
+		m_result = f1 * f2;
+		return m_result;
+	}
+
+	float div(float f1, float f2) {
+		m_result = f1 / f2;
+		return m_result;
+	}
+
+	void reset() {
+		m_result = 0;
+	}
+	
 };
--- calculatrice/CalculatriceScientifique.h
+++ calculatrice/CalculatriceScientifique.h
@@ -1,10 +1,27 @@
 #pragma once
 
 #include "CalculatriceNormale.h"
+#include <cassert>
 
 class CalculatriceScientifique : public CalculatriceNormale{
 public:
 	CalculatriceScientifique() {
 
+	}
+
+	float exponentielle(float f) {
+		m_result = exp(f);
+		return m_result;
+	}
+
+	float racine(float f) {
+		assert(f >= 0);
+		m_result = sqrt(f);
+		return m_result;
+	}
+
+	float puissance(float f, float p) {
+		m_result = pow(f, p);
+		return m_result;
 	}
 };
