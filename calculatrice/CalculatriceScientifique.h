c3af3640bb41c939ff18b164140083fd17d50d95
BrandonLeBon <brandonlebon@hotmail.fr>
2019-02-27 14:58:53 +0100

    Ajout du menu. \n *création du menu \n *création d'une classe normale et scientifique

---
+++ calculatrice/CalculatriceScientifique.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CalculatriceNormale.h"
+
+class CalculatriceScientifique : public CalculatriceNormale{
+public:
+	CalculatriceScientifique() {
+
+	}
+};
--- calculatrice/calculatrice.cpp
+++ calculatrice/calculatrice.cpp
@@ -3,13 +3,38 @@
 
 #include "calculatrice.h"
 #include "../dependencies/termcolor/include/termcolor/termcolor.hpp"
+#include "CalculatriceNormale.h"
+#include "CalculatriceScientifique.h"
+
+
 
 using namespace std;
 
+
 int main()
 {
-	while (true) {
-		cout << termcolor::red << "Hello CMake." << endl;
-	}  
+	cout << termcolor::red << "Choisissez le mode :" << endl
+	<< "1 - Mode normal" << endl
+	<< "2 - Mode scientifique" << endl;
+
+	int saisie;
+	cin >> saisie;
+	while (saisie!=1 && saisie!= 2) {
+		cout << termcolor::red << "Erreur de saisie. Veuillez resaisir le mode:" << endl
+		<< "1 - Mode normal" << endl
+		<< "2 - Mode scientifique" << endl;
+		cin >> saisie;
+	}
+
+	switch (saisie) {
+		case 1:
+			CalculatriceNormale calculatrice;
+			break;
+		case 2:
+			CalculatriceScientifique calculatrice;
+			break;
+	}
+
+
 	return 0;
 }
