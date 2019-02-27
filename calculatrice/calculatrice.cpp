8a1e96da1bb5b26099fc9d334958c4434d500dbf
BrandonLeBon <brandonlebon@hotmail.fr>
2019-02-27 11:27:10 +0100

    initial commit

---
+++ calculatrice/calculatrice.cpp
@@ -0,0 +1,12 @@
+﻿// calculatrice.cpp : définit le point d'entrée de l'application.
+//
+
+#include "calculatrice.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
